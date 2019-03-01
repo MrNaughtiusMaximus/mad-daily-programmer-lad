d7aaa7be229601ed945d80f20f9b9ee250b12135
Yordan Angelov <32510422+YordanAngelov@users.noreply.github.com>
2019-03-01 10:58:36 +0000

    added another function to c369e

--- src/main/scala/Challenge369Easy.scala
+++ src/main/scala/Challenge369Easy.scala
@@ -12,6 +12,19 @@ object Challenge369Easy {
     case a if a == 15  ⇒ "F"
   }
 
+  // More efficient than intToHex
+  def intToHex2(int: Int): String = {
+    if (int < 10) int.toString
+    else if (int == 10) "A"
+    else if (int == 11) "B"
+    else if (int == 12) "C"
+    else if (int == 13) "D"
+    else if (int == 14) "E"
+    else if (int == 15) "F"
+    else "Fuck!"
+  }
+
+  // The most efficient way of converting
   val hexIndex: List[String] = List("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
 
   def convertToHex(i1: Int, i2: Int, i3: Int): String =
